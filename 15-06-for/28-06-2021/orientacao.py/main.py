fac5775b687cb84f9cef9366ca7f17825f9917f4
João Vitor Gomes <joaovitorgomes95@gmail.com>
2021-07-06 22:32:44 -0300

    ok

--- 15-06-for/28-06-2021/orientacao.py/main.py
+++ 15-06-for/28-06-2021/orientacao.py/main.py
@@ -1,49 +1,52 @@
+from serie import Serie
+from filme import Filme
 
-# Isso serve para garantir que o código desse arquivo só será executado no arquivo principal
-# if __name__ == '__main__':
-#     pass
-class Conta:
-    def __init__(self, titular, saldo=0):
-        self.titular = titular
-        self.saldo = saldo
+# filme = Filme('Guerra do Amanhã', 2021, 160)
+# serie = Serie('Young Sheldon', 2017, 3)
+catalogo = list()
+while True:
 
-    def __str__(self):
-        if self.saldo == 0:
-            return f'''
-            Titular:{self.titular}
-            '''
-        else:
-            return f'''
-            Titular: {self.titular}
-            Saldo: {self.saldo}
-            '''
+    cadastro = input(
+        'O que você quer cadastrar?: Filme/Serie').strip().upper()[0]
+    while cadastro not in 'FS':
+        cadastro = input(
+            'O que você quer cadastrar?: Filme/Serie').strip().upper()[0]
 
-    def depositar(self, valor):
-        self.saldo += valor
-        return f'{self.titular} seu saldo atual é {self.saldo}'
+    if cadastro == 'F':
+        filme = Filme(
+            input('Nome: '),
+            int(input('Ano: ')),
+            int(input('Duração: '))
+        )
+        catalogo.append(filme)
+    elif cadastro == 'S':
+        serie = Serie(
+            input('Nome: '),
+            int(input('Ano: ')),
+            int(input('Duração: '))
+        )
+    catalogo.append(serie)
+    continua = input('Você quer continuar: [S/N]')
+    while continua not in 'SN':
+        continua = input('Digite S ou N: ')
 
-    def sacar(self, valor):
-        if valor > 0:
-            if self.saldo < valor:
-                print(f'{self.titular} você não tem {valor:.f} para sacar. ')
-                self.extrato()
-            else:
-                print('Você não pode sacar 0 reais!')
+    if continua == 'N':
+        break
+
+for i in catalogo:
+    print(i)
+    like = input('Você quer dar o like nesse item? [S/N] ').strip().upper()[0]
+    while continua not in 'SN':
+        like = input(
+            'Você quer dar o like nesse item? [S/N] ').strip().upper()[0]
+
+    if like == 'S':
+        i.dar_like()
+        print('Você curtiu esse conteúdo')
+
+    else:
+        print('Ok!')
 
 
-if __name__ == '__main__':
-
-    banco = list()
-
-    while True:
-        conta = Conta(input('Titular da conta: '))
-        banco.append(conta)
-
-        resp = input('Deseja continuar: [S/N]:').upper().strip()[0]
-        if resp == 'N':
-            break
-
-    print(banco[1].depositar(50))
-
-    for o in banco:
-        print(o)
+for i in catalogo:
+    print(i)
