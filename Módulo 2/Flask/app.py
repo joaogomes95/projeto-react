9cbae798479700c463e939a2e2de5af479ea0ed5
João Vitor Gomes <joaovitorgomes95@gmail.com>
2021-07-21 22:22:16 -0300

    ok

---
+++ Módulo 2/Flask/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+itens = []
+
+
+@app.route('/')
+def index():
+    nomelista = "Lista de coisas a fazer!"
+    listapronta = True
+    return render_template('index.html',
+                           nomelista=nomelista, listapronta=listapronta)
+
+
+@app.route('/nova', methods=['GET', 'POST'])
+def nova():
+    if request.method == 'POST':
+        item = request.form['item']
+        itens.append(item)
+        return redirect('/')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
