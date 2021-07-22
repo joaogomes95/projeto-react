7382b07b52c8a85f3325bfb07dfb97af67d530d0
João Vitor Gomes <joaovitorgomes95@gmail.com>
2021-07-21 22:22:37 -0300

    ok

---
+++ Módulo 2/Exercícios/Menu com 3 escolhas/script.js
@@ -0,0 +1,14 @@
+function funcao1()
+{
+alert("Você escolheu virada paulista!");
+}
+
+function funcao2()
+{
+alert("Você escolheu frango frito!");
+}
+
+function funcao3()
+{
+alert("Você escolheu Feijoada!");
+}
--- Módulo 2/Projetos/Projeto 1 - Mudança de Humor/script.js
+++ Módulo 2/Projetos/Projeto 1 - Mudança de Humor/script.js
@@ -1,5 +1,21 @@
-function img01 () {
-    document.getElementById("trocarimg").src="feliz.gif";
+// function img01 () {
+//     document.getElementById("trocarimg").src="feliz.gif";
+// }
+
+const btn = document.getElementById("botao");
+const img = document.getElementById("trocarimg");
+
+btn.onclick = function () {
+    if(btn.value === 'img1'){
+        img.src="feliz.gif";
+        btn.value = 'img2';
+    }
+    else if (btn.value === 'img2'){
+        img.src="triste.gif";
+        btn.value = 'img1'
+    }
 }
 
 
+// Adicione um campo de formulário no final com os itens: 
+// nome, endereço, e forma de pagamento (escolher entre dinheiro ou cartão)
